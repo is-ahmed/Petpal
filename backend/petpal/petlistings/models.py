18b2de0327a69ac543ba8d81a62e1a0da2d92544
Ismail Ahmed <isahmed@pop-os.localdomain>
2023-11-05 21:50:52 -0500

    Add some starter code for pet listings app

--- backend/petpal/petlistings/models.py
+++ backend/petpal/petlistings/models.py
@@ -1,1 +1,12 @@
+from django.db import models
 
+# Create your models here.
+class Pet(models.Model):
+    name=models.CharField(max_length=120) # TODO: Double check this
+    age=models.IntegerField()
+    breed=models.CharField(max_length=120)
+    species=models.CharField(max_length=120)
+    gender=models.CharField(max_length=120)
+    image=models.ImageField(upload_to="uploads", null=True)
+    available=models.BooleanField(max_length=120)
+    #shelter=models.ForeignKey(Shelter, on_delete=models.SET_NULL, null=True)  # Need Jeff to finish his stuff for this
