17b9086405ec2f36c19d306a579f15346413caa2
Ismail Ahmed <isahmed@pop-os.localdomain>
2023-11-12 00:12:55 -0500

    Add Notifications app

--- backend/petpal/petpal_backend/settings.py
+++ backend/petpal/petpal_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'petlistings',
     'django_filters',
+    'notifications',
 ]
 
 MIDDLEWARE = [
--- backend/petpal/petpal_backend/urls.py
+++ backend/petpal/petpal_backend/urls.py
@@ -23,4 +23,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('petlistings/', include('petlistings.urls', namespace="petlistings")),
+    path('notifications/', include('notifications.urls', namespace="notifications")),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
