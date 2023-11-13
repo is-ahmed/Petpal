9300484186d266eab366754a071b1a51ef530d64
Percy <82243197+junwjie@users.noreply.github.com>
2023-11-13 04:39:53 -0500

    Applcation model mostly finished

--- backend/petpal/petlistings/urls.py
+++ backend/petpal/petlistings/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import PetsListCreate, PetRetrieveUpdateDestroy
+from applications.views import ApplicationCreateAPIView
 
 app_name="petlistings"
 urlpatterns = [
     path("pets", PetsListCreate.as_view(), name="petlist"),
     path("pets/<int:pk>", PetRetrieveUpdateDestroy.as_view(), name="pet"),
+    path("pets/<int:pet_id>/applications/", ApplicationCreateAPIView.as_view(), name="application-create"),
 ]
