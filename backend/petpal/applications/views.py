00028e563e9c0ddea522dd0c4c8cc6ed53f0bdb9
junjiewang0000@gmail.com <junjiewang0000@gmail.com>
2023-11-14 16:41:06 -0500

    Applications done

--- backend/petpal/applications/views.py
+++ backend/petpal/applications/views.py
@@ -9,14 +9,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 from petlistings.models import Pet
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import RetrieveAPIView
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, PermissionDenied
 from notifications.models import Notification
 from datetime import datetime
 
 class CustomPagination(PageNumberPagination):
-    page_size = 2  # Set your desired page size here
-    page_size_query_param = 'page_size'  # Allow client to override the page size using this query parameter
-    max_page_size = 2  # Maximum limit for page size
+    page_size = 10  
+    page_size_query_param = 'page_size'  
+    max_page_size = 12  
 
 class ApplicationView(RetrieveAPIView):
     #queryset = Application.objects.all()
@@ -39,7 +39,7 @@ class ApplicationListView(ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['status']
     ordering_fields = ['creation_time', 'last_update_time']
-    pagination_class = PageNumberPagination
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         # Assuming 'user' is a ForeignKey to the Shelter in the Application model
@@ -59,13 +59,23 @@ class ApplicationCreateAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         #pet_listing = serializer.validated_data.get('pet_listing')
+        user = self.request.user
+        if user.account_type == 'shelter':
+            raise PermissionDenied({'message': 'Shelters cannot apply to adopt pets.'})
         pet_id = self.kwargs.get('pet_id')
         try:
             #print("Works")
             pet_listing = Pet.objects.get(id=pet_id, status='available')
         except Pet.DoesNotExist:
             #print("No pet")
-            raise ValidationError({'message': 'Pet listing not available or does not exist.'})
+            raise PermissionDenied({'message': 'Pet listing not available or does not exist.'})
+        
+        try:
+            applicationExists = Application.objects.get(pet_listing=pet_listing, user=user)
+            raise PermissionDenied({'message': 'You already have an application for this pet.'})
+        except Application.DoesNotExist:
+            pass
+
         serializer.save(user=self.request.user, pet_listing=pet_listing, shelter=pet_listing.shelter.user)
 
 class ApplicationUpdateAPIView(UpdateAPIView):
@@ -94,28 +104,49 @@ class ApplicationRetrieveUpdateView(RetrieveUpdateAPIView):
             return ApplicationUpdateSerializer
         return ApplicationSerializer
 
-    def get_queryset(self):
+    # def get_object(self):
+    #     application = super().get_object()  # Get the object using the default implementation
+    #     user = self.request.user
+
+    def get_object(self):
+        # Retrieve the object using the default implementation
+        obj = super().get_object()
+
         user = self.request.user
-        if user.account_type == 'seeker':
-            return Application.objects.filter(user=user)
-        elif user.account_type == 'shelter':
-            return Application.objects.filter(shelter=user)
-        return Application.objects.none()  #If not logged in just return nothing
+
+        # Check permissions based on user account type
+        if user.account_type == 'seeker' and user != obj.user:
+            raise PermissionDenied("Seekers can only access their own applications.")
+        elif user.account_type == 'shelter' and user != obj.shelter:
+            raise PermissionDenied("Shelters can only access applications related to them.")
+
+        return obj
+    
+    def get_queryset(self):
+        return Application.objects.all()
+        # user = self.request.user
+        
+        # if user.account_type == 'seeker':
+        #     return Application.objects.filter(user=user)
+        # elif user.account_type == 'shelter':
+        #     return Application.objects.filter(shelter=user)
+        # return Application.objects.none()  #If not logged in just return nothing
 
     def perform_update(self, serializer):
         application = self.get_object()
         user = self.request.user
 
-        # if application.user != user and application.shelter != user.shelter:
-        #     raise ValidationError({'message':"You do not have permission to update this application."})
+
+        # if not (user == application.user or user == application.shelter):
+        #     raise PermissionDenied({'message':"You do not have permission to update this application."})
         
         orig_status = application.status
         if user.account_type == 'shelter':
             if application.status != 'pending':
-                raise ValidationError({'status': 'You can only modify applications that are pending.'})
+                raise PermissionDenied({'You can only modify applications that are pending.'})
         elif user.account_type == 'seeker':
             if not (application.status == 'pending' or application.status == 'accepted'):
-                raise ValidationError({'status': 'You can only modify applications that are pending or accepted.'})
+                raise PermissionDenied({'You can only modify applications that are pending or accepted.'})
         
         serializer.save()
         updated_status = serializer.instance.status
