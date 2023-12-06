9af885b09e2acecf5684cd12b3889aa556dc4114
Ismail Ahmed <isahmed@pop-os.localdomain>
2023-12-06 16:26:35 -0500

    Add pages

---
+++ frontend/petpal/src/pages/RegisterSuccess/index.jsx
@@ -0,0 +1,19 @@
+import LandingHeader from "../../components/LandingHeader";
+import Footer from "../../components/Footer";
+import SignUpBody from "../../components/SignUpBody";
+
+const RegisterSuccess = () => {
+	return (
+		<>
+		<LandingHeader/>
+		<main class="d-flex flex-column justify-content-center align-items-center ">
+		<h1 class="text-white">Thanks for joining Petpal!</h1>	
+		</main>
+
+		<Footer/>
+		</>
+	)
+
+}
+
+
---
+++ frontend/petpal/src/pages/SearchPets/index.jsx
@@ -0,0 +1,63 @@
+import LandingHeader from "../../components/LandingHeader";
+import Footer from "../../components/Footer";
+import { useSearchParams } from "react-router-dom";
+import { useState, useEffect, useMemo } from "react";
+import SearchFilters from "../../components/SearchFilters";
+import PetSearchResults from "../../components/PetSearchResults";
+import { SearchContext } from "../../contexts/SearchContext";
+import Navigation from "../../components/Navigation";
+
+
+
+const Pets = () => {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const [petResults, setPetResults] = useState();
+	const [getResults, setResultsFinished] = useState(0);
+
+	const query = useMemo(() => ({
+		page : parseInt(searchParams.get("page") ?? 1),
+		page_size: 6,
+		ordering: searchParams.get("ordering") ?? 'Name',
+		shelter: parseInt(searchParams.get('shelter') ?? -1),
+		species: searchParams.get('species') ?? '',
+		age: parseInt(searchParams.get('age') ?? 1),
+		status: searchParams.get('status') ?? 'all',
+		daysOnPetPal: parseInt(searchParams.get('daysOnPetpal') ?? 0),
+		gender: searchParams.get('gender') ?? '',
+		breed: searchParams.get('breed') ?? ''
+	}), [searchParams]);
+
+	useEffect(() => {
+		async function fetchPets() {
+			const { page, page_size, ordering, shelter, species, age, status, daysOnPetPal, gender, breed } = query;
+			const response = await fetch(`http://localhost:8000/petlistings/pets?p=${page}&page_size=6&status=all&ordering=${ordering}&shelter=${shelter}&species=${species}&status=${status}&gender=${gender}&breed=${breed}`, {
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('access_token')}`
+				}
+			});
+			const pets = await response.json()
+			return pets;
+		}
+		fetchPets().then(json => {
+			setPetResults(json['results'])
+			setResultsFinished(1);
+		})
+		.catch(err => console.log(err))
+	}, [query]) // dependent on query
+
+	return (
+		<>
+		<Navigation type='seeker' username='username'/>
+		<main className="d-flex flex-grow-1">
+		<SearchContext.Provider value={{query, searchParams, setSearchParams}}>	
+			<SearchFilters modal={false}/>
+		{getResults ? <PetSearchResults results={petResults}/> : ""}	
+		</SearchContext.Provider>
+		</main>	
+		<Footer/>
+		</>
+	)
+}
+
+export default Pets;
