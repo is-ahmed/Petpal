810999394b9b4017abedd99e72c52a476e331bd6
erji2022 <erkhjin@gmail.com>
2023-12-07 17:06:32 -0500

    app.js update

--- frontend/petpal/src/App.js
+++ frontend/petpal/src/App.js
@@ -1,26 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Pet from './pages/Pet';
+import UserUpdatePage  from './pages/UserUpdate';
+import ShelterUpdatePage from './pages/ShelterUpdate';
+import UserUpdateSuccessPage from './pages/UserSuccess';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <BrowserRouter>
+    <Routes>
+      <Route path="pet/:petId/" element={<Pet />} />
+      <Route path="seeker/update" element={<UserUpdatePage />}/>
+      <Route path="shelter/update" element={<ShelterUpdatePage />}/>
+      <Route path="success" element={<UserUpdateSuccessPage/>}/>
+ 
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
