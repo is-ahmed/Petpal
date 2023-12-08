ac1f65d371b194ee267ec0ca8b402ace64b13aeb
jeffhu <jeff1hu@gmail.com>
2023-12-08 13:01:33 -0500

    c

--- frontend/petpal/src/components/PetCard.jsx
+++ frontend/petpal/src/components/PetCard.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import styles from './css/search.module.css'
 
-const PetCard = ({pet}) => {
+const PetCard = ({pet, flagjeff}) => {
 	
 	useEffect(() => {
 	}, [])
 
 	return (
-		<a className={`col mt-3 me-3 mb-3 shadow rounded ${styles.petinfo} p-3`} href={`http://localhost:3000/pet/${pet['id']}`}>
+		<a className={`${!flagjeff && 'col'} mt-3 me-3 mb-3 shadow rounded ${styles.petinfo} p-3`} href={`http://localhost:3000/pet/${pet['id']}`}
+		style={{
+			width: flagjeff && '30%'
+		}}>
 				<img
 				  src={pet['image']}
 				  className="img-fluid rounded"
