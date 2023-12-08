d3edd7870dc733289b1398d7e2f119226fe2b1cf
Ismail Ahmed <isahmed@pop-os.localdomain>
2023-12-08 09:52:10 -0500

    Add account type to report serializer

--- backend/petpal/reports/serializers.py
+++ backend/petpal/reports/serializers.py
@@ -5,7 +5,8 @@ from rest_framework.serializers import CharField, ReadOnlyField
 class ReportSerializer(serializers.ModelSerializer):
     id = ReadOnlyField()
     name = CharField(source = 'subject.username', read_only=True)
+    subject_type = CharField(source='subject.account_type', read_only=True)
     class Meta:
         model = Report
-        fields = ['id', 'subject', 'status', 'description', 'name']
+        fields = ['id', 'subject', 'status', 'description', 'name', 'subject_type']
         read_only_fields = ['author']
