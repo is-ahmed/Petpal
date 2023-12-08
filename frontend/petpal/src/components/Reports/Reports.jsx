f3876616820748c6e4889d386e435f9c5c6ae7ea
jeffhu <jeff1hu@gmail.com>
2023-12-08 11:14:42 -0500

    jeff

---
+++ frontend/petpal/src/components/Reports/Reports.jsx
@@ -0,0 +1,287 @@
+import {useEffect, useState} from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.sm.css'
+import './shelter-manager.sm.css'
+import {Button, CloseButton, Dropdown, Modal, Navbar} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronLeft, faChevronRight, faEllipsis, faPlus} from '@fortawesome/free-solid-svg-icons'
+
+export function Reports() {
+    const [shelterDetails, setShelterDetails] = useState({})
+    const [pets, setPets] = useState([])
+    const [petDelete, setPetDelete] = useState(0) // will hold the id of the pet to delete
+    const [error, setError] = useState('')
+    const [applications, setApplications] = useState({})
+
+    const [showPetAppName, setShowPetAppName] = useState('')
+    const [showPetAppId, setShowPetAppId] = useState()
+    const [showPetApp, setShowPetApp] = useState(false)
+
+    const closePetApp = () => setShowPetApp(false)
+
+    const [accountType ,setAccountType] = useState('')
+    const [subjectID ,setSubjectID] = useState('')
+
+    const cancelDelete = () => setPetDelete(0)
+
+
+    const getPetApplications = () => {
+        fetch(`http://localhost:8000/applications/`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        })
+            .then(response => response.json())
+            .then(json => {
+                let result_object = {}
+                for (let obj of json.results) {
+                    if (!(obj.pet_listing in result_object)) {
+                        result_object[obj.pet_listing] = []
+                    }
+
+                    result_object[obj.pet_listing].push(obj)
+                }
+
+                setApplications(result_object)
+            })
+    }
+
+    const showPetApplications = (id, name) => {
+        setShowPetAppName(name)
+        setShowPetApp(true)
+        setShowPetAppId(id)
+    }
+
+    useEffect(() => {
+        if ('id' in shelterDetails) {
+        }
+    }, [shelterDetails]);
+
+    const [page, setPage] = useState(1)
+    const [totalPage, setTotalPage] = useState(1)
+    const [reports, setReports] = useState([])
+    const [description, setDescription] = useState('')
+    const [id, setID] = useState(NaN)
+    const [name, setName] = useState('')
+    const [visible, setVisible] = useState(false)
+
+    const show = () => {
+        setVisible(true)
+    }
+
+    const hide = () => {
+        setVisible(false)
+    }
+
+    const [k, setK] = useState(0)
+    useEffect(() => {
+        fetch(`http://localhost:8000/reports/?page=${page}&page_size=10`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        })
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                setReports(json.results)
+                setTotalPage(Math.ceil(json.count / 10))
+            })
+    }, [page, k])
+
+    const makeDate = (days_on_petpal) => {
+        let date = new Date()
+        date.setDate(date.getDate() - days_on_petpal)
+        console.log(date, date.getDay())
+        return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+    }
+    return <>
+        {(error) ? <p>{error}</p>:
+            <main className="page-container">
+                <h1 className="management-title">Admin Panel</h1>
+                <hr/>
+                <div className="pet-list-title">
+                    <h3>Reports:</h3>
+                    <div className="button-add-pet">
+                        {reports.length !== 0 &&
+                            <div className="page-button-container"
+                                 style={{marginRight: 'auto'}}>
+                                <Button variant={'link'}
+                                        className={'page-button'} disabled={page <= 1}
+                                        onClick={() => {setPage(page - 1)}}>
+                                    <FontAwesomeIcon icon={faChevronLeft}/>
+                                </Button>
+
+                                <Button variant={'link'}
+                                        className={'page-button'} disabled={page >= totalPage}
+                                        onClick={() => {setPage(page + 1)}}>
+                                    <FontAwesomeIcon icon={faChevronRight}/>
+                                </Button>
+
+                                <span>Page: {page}/{totalPage}</span>
+                            </div>}
+
+                    </div>
+                </div>
+                <div className="pet-list-holder">
+                    {reports.map((report, i) => {
+                        return <Button variant={'link'}
+                                       style={{textDecoration: 'none'}}
+                                       className="pet-list-card" key={`petlist${i}`}
+                        disabled={report.status === 'accepted'}
+                        onClick={() => {
+                            setDescription(report.description)
+                            setName(report.name)
+                            setID(report.id)
+                            setSubjectID(report.subject)
+                            setAccountType(report.subject_type)
+
+                            show()
+                        }}>
+                            <p className="pet-name">{report.name}</p>
+                            <div className="applicant-message-holder">
+                                <p className="applicant-message">
+                                    {report.description}
+                                </p>
+                            </div>
+                        </Button>
+                    })}
+                </div>
+
+                <Modal show={visible} onHide={hide}>
+                    <Modal.Header>
+                        <Modal.Title className={'fs-5'}>Report for {name}</Modal.Title>
+                        <CloseButton onClick={hide}/>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h6>Report description</h6>
+                        <p>{description}</p>
+                        <Button style={{width: '100%'}}variant={'link'}
+                        href={`/${accountType ? 'shelters' : 'temp'}/${subjectID}`}>View Account</Button>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant={'secondary'} onClick={hide}>Cancel</Button>
+                        <Button variant={'danger'} onClick={() => {
+                            fetch(`http://localhost:8000/reports/${id}/`,
+                                {
+                                    method: 'patch',
+                                    headers: {
+                                        Authorization: `Bearer ${localStorage.getItem('access_token')}`
+                                    },
+                                    body: {
+                                        status: 'accepted'
+                                    }
+                                })
+                            hide()
+                            setK(k + 1)
+                        }}>Ban</Button>
+                    </Modal.Footer>
+                </Modal>
+                <Modal show={showPetApp} onHide={closePetApp}>
+                    <Modal.Header>
+                        <Modal.Title>Applicants for {showPetAppName}</Modal.Title>
+                        <CloseButton onClick={closePetApp}/>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="applicants-card-holder">
+                            {showPetAppId in applications ? (
+                                applications[showPetAppId].map((pet, i) => (
+                                    <Button href={'temp'} variant={'link'} className={'applicants-card'} key={i}>
+                                        <h3 className="applicant-name">Josh</h3>
+                                        <div className="applicant-message-holder">
+                                            <p className="applicant-message">
+                                                Blah blah blah
+                                            </p>
+                                        </div>
+
+                                    </Button>
+                                ))
+                            ) : (
+                                <p>No current applications!</p>
+                            )}
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={closePetApp} variant={'primary'}>Done</Button>
+                    </Modal.Footer>
+                </Modal>
+
+
+                <div
+                    className="modal fade"
+                    id="applicants-page"
+                    aria-labelledby="edit-pet-label"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog modal-dialog-scrollable">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="edit-pet-label">
+                                    Applicants for Ben
+                                </h1>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                />
+                            </div>
+                            <div className="modal-body">
+                                <div className="applicants-card-holder">
+                                    <a
+                                        className="btn btn-link applicants-card"
+                                        href="petapplicationShelterView.html"
+                                    >
+                                        <h3 className="applicant-name">John Doe</h3>
+                                        <p className="applicant-message">
+                                            Thank you for your prompt response. I am eager to welcome a new
+                                            member to our family!
+                                        </p>
+                                    </a>
+                                    <a
+                                        className="btn btn-link applicants-card"
+                                        href="petapplicationShelterView.html"
+                                    >
+                                        <h3 className="applicant-name">John Doe</h3>
+                                        <p className="applicant-message">
+                                            Thank you for your prompt response. I am eager to welcome a new
+                                            member to our family!
+                                        </p>
+                                    </a>
+                                    <a
+                                        className="btn btn-link applicants-card"
+                                        href="petapplicationShelterView.html"
+                                    >
+                                        <h3 className="applicant-name">John Doe</h3>
+                                        <p className="applicant-message">
+                                            Thank you for your prompt response. I am eager to welcome a new
+                                            member to our family!
+                                        </p>
+                                    </a>
+                                    <a
+                                        className="btn btn-link applicants-card"
+                                        href="petapplicationShelterView.html"
+                                    >
+                                        <h3 className="applicant-name">John Doe</h3>
+                                        <p className="applicant-message">
+                                            Thank you for your prompt response. I am eager to welcome a new
+                                            member to our family!
+                                        </p>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Done
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>}
+    </>
+}
---
+++ frontend/petpal/src/components/ShelterList/ShelterList.jsx
@@ -0,0 +1,290 @@
+import {useEffect, useState} from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.sm.css'
+import './shelter-manager.sm.css'
+import {Button, CloseButton, Dropdown, Modal, Navbar} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronLeft, faChevronRight, faEllipsis, faPlus} from '@fortawesome/free-solid-svg-icons'
+
+export function ShelterList() {
+    const [shelterDetails, setShelterDetails] = useState({})
+    const [pets, setPets] = useState([])
+    const [petDelete, setPetDelete] = useState(0) // will hold the id of the pet to delete
+    const [error, setError] = useState('')
+    const [applications, setApplications] = useState({})
+
+    const [showPetAppName, setShowPetAppName] = useState('')
+    const [showPetAppId, setShowPetAppId] = useState()
+    const [showPetApp, setShowPetApp] = useState(false)
+
+    const closePetApp = () => setShowPetApp(false)
+
+    const cancelDelete = () => setPetDelete(0)
+
+    const getPets = () => {
+        fetch(`http://localhost:8000/petlistings/pets?shelter=${shelterDetails.id}&status=all`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw new Error(`${response.status}: ${response.statusText}`)
+            })
+            .then(json => {
+                setPets(json.results)
+            })
+            .catch(error => {
+                setError(error.toString())
+            })
+    }
+
+    const getPetApplications = () => {
+        fetch(`http://localhost:8000/applications/`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        })
+            .then(response => response.json())
+            .then(json => {
+                let result_object = {}
+                for (let obj of json.results) {
+                    if (!(obj.pet_listing in result_object)) {
+                        result_object[obj.pet_listing] = []
+                    }
+
+                    result_object[obj.pet_listing].push(obj)
+                }
+
+                setApplications(result_object)
+            })
+    }
+
+    const showPetApplications = (id, name) => {
+        setShowPetAppName(name)
+        setShowPetApp(true)
+        setShowPetAppId(id)
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:8000/shelter', {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw new Error(`${response.status}: ${response.statusText}`)
+            })
+            .then(json => {
+                setShelterDetails(json)
+            })
+            .catch(error => {
+                setError(error.toString())
+            })
+
+        getPetApplications()
+    }, []);
+
+    useEffect(() => {
+        if ('id' in shelterDetails) {
+            getPets()
+        }
+    }, [shelterDetails]);
+
+    const [page, setPage] = useState(1)
+    const [totalPage, setTotalPage] = useState(1)
+    const [shelters, setShelters] = useState([])
+    useEffect(() => {
+        fetch(`http://localhost:8000/shelters/?page=${page}&page_size=10`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        })
+            .then(res => res.json())
+            .then(json => {
+                setShelters(json.results)
+                setTotalPage(Math.ceil(json.count / 10))
+            })
+    }, [page])
+
+    const makeDate = (days_on_petpal) => {
+        let date = new Date()
+        date.setDate(date.getDate() - days_on_petpal)
+        console.log(date, date.getDay())
+        return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+    }
+    return <>
+        {(error) ? <p>{error}</p>:
+            <main className="page-container">
+                <h1 className="management-title">PetPal Shelters</h1>
+                <hr/>
+                <div className="pet-list-title">
+                    <h3>Shelters:</h3>
+                    <div className="button-add-pet">
+                        {shelters.length !== 0 &&
+                            <div className="page-button-container"
+                                 style={{marginRight: 'auto'}}>
+                                <Button variant={'link'}
+                                        className={'page-button'} disabled={page <= 1}
+                                        onClick={() => {setPage(page - 1)}}>
+                                    <FontAwesomeIcon icon={faChevronLeft}/>
+                                </Button>
+
+                                <Button variant={'link'}
+                                        className={'page-button'} disabled={page >= totalPage}
+                                        onClick={() => {setPage(page + 1)}}>
+                                    <FontAwesomeIcon icon={faChevronRight}/>
+                                </Button>
+
+                                <span>Page: {page}/{totalPage}</span>
+                            </div>}
+
+                    </div>
+                </div>
+                <div className="pet-list-holder">
+                    {shelters.map((shelter, i) => {
+                        return <Button variant={'link'}
+                                       style={{textDecoration: 'none'}}
+                                       href={`/shelters/${shelter.shelter_id}`}
+                                       className="pet-list-card" key={`petlist${i}`}>
+                            <img src={shelter.avatar}/>
+                            <p className="pet-name">{shelter.name}</p>
+                        </Button>
+                    })}
+                </div>
+                <Modal show={petDelete} onHide={cancelDelete}>
+                    <Modal.Header>
+                        <Modal.Title className={'fs-5'}>Are you sure you want to remove this pet?</Modal.Title>
+                        <CloseButton onClick={cancelDelete}/>
+                    </Modal.Header>
+                    <Modal.Body>
+                        You won't be able to undo this!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant={'secondary'} onClick={cancelDelete}>Cancel</Button>
+                        <Button variant={'primary'} onClick={() => {
+                            fetch(`http://localhost:8000/petlistings/pets/${petDelete}`, {
+                                method: 'DELETE',
+                                headers: {
+                                    'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+                                }
+                            })
+                                .then(() => {
+                                    getPets()
+                                    setPetDelete(0)
+                                })
+                        }}>Confirm</Button>
+                    </Modal.Footer>
+                </Modal>
+                <Modal show={showPetApp} onHide={closePetApp}>
+                    <Modal.Header>
+                        <Modal.Title>Applicants for {showPetAppName}</Modal.Title>
+                        <CloseButton onClick={closePetApp}/>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="applicants-card-holder">
+                            {showPetAppId in applications ? (
+                                applications[showPetAppId].map((pet, i) => (
+                                    <Button href={'temp'} variant={'link'} className={'applicants-card'} key={i}>
+                                        <h3 className="applicant-name">Josh</h3>
+                                        <p className="applicant-message">
+                                            Blah blah blah
+                                        </p>
+                                    </Button>
+                                ))
+                            ) : (
+                                <p>No current applications!</p>
+                            )}
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={closePetApp} variant={'primary'}>Done</Button>
+                    </Modal.Footer>
+                </Modal>
+
+
+                <div
+                    className="modal fade"
+                    id="applicants-page"
+                    aria-labelledby="edit-pet-label"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog modal-dialog-scrollable">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="edit-pet-label">
+                                    Applicants for Ben
+                                </h1>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                />
+                            </div>
+                            <div className="modal-body">
+                                <div className="applicants-card-holder">
+                                    <a
+                                        className="btn btn-link applicants-card"
+                                        href="petapplicationShelterView.html"
+                                    >
+                                        <h3 className="applicant-name">John Doe</h3>
+                                        <p className="applicant-message">
+                                            Thank you for your prompt response. I am eager to welcome a new
+                                            member to our family!
+                                        </p>
+                                    </a>
+                                    <a
+                                        className="btn btn-link applicants-card"
+                                        href="petapplicationShelterView.html"
+                                    >
+                                        <h3 className="applicant-name">John Doe</h3>
+                                        <p className="applicant-message">
+                                            Thank you for your prompt response. I am eager to welcome a new
+                                            member to our family!
+                                        </p>
+                                    </a>
+                                    <a
+                                        className="btn btn-link applicants-card"
+                                        href="petapplicationShelterView.html"
+                                    >
+                                        <h3 className="applicant-name">John Doe</h3>
+                                        <p className="applicant-message">
+                                            Thank you for your prompt response. I am eager to welcome a new
+                                            member to our family!
+                                        </p>
+                                    </a>
+                                    <a
+                                        className="btn btn-link applicants-card"
+                                        href="petapplicationShelterView.html"
+                                    >
+                                        <h3 className="applicant-name">John Doe</h3>
+                                        <p className="applicant-message">
+                                            Thank you for your prompt response. I am eager to welcome a new
+                                            member to our family!
+                                        </p>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Done
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>}
+    </>
+}
